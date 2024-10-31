a6d289f991f7b26f2186fa4f3a2460e4b10f240a
Aryan Thakur <137707937+PIXEL667@users.noreply.github.com>
2024-10-31 15:03:53 +0530

    commit

--- notes2.cpp
+++ notes2.cpp
@@ -2,14 +2,18 @@
 
 int main()
 {
-/*
-^.const :
-!.The const keyword specifies that a variable's values is constant
-!.Tells the compiler to prevent anything from modifing it
-!.(READ ONLY)
-*/
+    /*
+    ^.const :
+    !.The const keyword specifies that a variable's values is constant
+    !.Tells the compiler to prevent anything from modifing it
+    !.(READ ONLY)
+    */
 
+    double pi = 3.14159;
+    double radius = 10;
+    double circumference = 2 * pi * radius;
 
+    std::cout << circumference << "cm";
 
-
+    return 0;
 }
